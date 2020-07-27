fc7f90490e7f17cd8607f80264c022f9c049785b
lkinnear <s0933963@sms.ed.ac.uk>
2020-07-27 13:30:58 +0100

    adding folders that didn't happen last time

---
+++ custom_climate_runs/custom_climate_full_workflow.py
@@ -0,0 +1,119 @@
+# Python imports
+import logging
+
+# Libs
+import geopandas as gpd
+import pandas as pd
+import shapely.geometry as shpg
+
+# Locals
+import oggm.cfg as cfg
+from oggm import utils, workflow, tasks
+
+
+# For timing the run
+import time
+start = time.time()
+
+# Module logger
+log = logging.getLogger(__name__)
+
+# Initialize OGGM and set up the default run parameters
+cfg.initialize(logging_level='WORKFLOW')
+rgi_version = '61'
+rgi_region = '13'
+
+# This has thrown errors sometimes when I've reduced it so left at 80
+cfg.PARAMS['border'] = 80
+# Set this to make the run look for the ref_t* csv in the workflow directory instead of downloading it
+cfg.PARAMS['run_mb_calibration'] = True
+# Use multiprocessing?
+cfg.PARAMS['use_multiprocessing'] = True
+cfg.PARAMS['mp_processes'] = 32
+# We are using which baseline data?
+cfg.PARAMS['baseline_climate'] = 'CUSTOM'
+# change the custom climate data
+cfg.PATHS['climate_file'] = '/exports/csce/datastore/geos/groups/geos_iceocean/kinnear/SWARM_files/oggm_SWARM_input_cru_ref_hgts/oggm_cru_hgt_input.nc'
+# Set to True for operational runs - here we want all glaciers to run
+cfg.PARAMS['continue_on_error'] = True
+
+# Local working directory (where OGGM will write its output) Need to create this beforehand in order to put the mass balance data in.
+WORKING_DIR = '/exports/csce/datastore/geos/groups/geos_iceocean/kinnear/oggm_runs/oggm_custom_climate_cru_rf_hgt'
+cfg.PATHS['working_dir'] = WORKING_DIR
+
+# RGI file
+path = utils.get_rgi_region_file(rgi_region, version=rgi_version)
+rgidf = gpd.read_file(path)
+
+#print(rgidf)
+# Get the shapefile
+basin = gpd.read_file('/exports/csce/datastore/geos/groups/geos_iceocean/kinnear/SWARM-OGGM-Model/shape_files/swarm_grid.shp')
+print('got glacier shp')
+
+# Take all glaciers in the desired areas
+in_bas = [basin.geometry.contains(shpg.Point(x, y))[0] for
+          (x, y) in zip(rgidf.CenLon, rgidf.CenLat)]
+rgidf = rgidf.loc[in_bas]
+
+
+
+# Sort for more efficient parallel computing
+rgidf = rgidf.sort_values('Area', ascending=False)
+# Get rid of smaller glaciers, area is in km^2
+rgidf = rgidf[rgidf.Area >= 1.0]
+#rgidf = rgidf.drop(rgidf[rgidf.Area < 1.0].index)
+
+
+
+##Added in section to convert to pandas dataframe and print out file if needed/check data
+#rgidf.to_file('/Users/louis/workflow_test/rgidf.shp')
+
+
+##Convert to pandas dataframe then print
+#rgipdf = pd.DataFrame(rgidf)
+#rgipdf.to_csv('/Users/louis/workflow_test/rgipdf.csv')
+
+#Start the run
+log.workflow('Starting OGGM run')
+log.workflow('Number of glaciers: {}'.format(len(rgidf)))
+
+#Go get the pre-processed glacier directories, this will eventually be reduced to prepro = 1 but for now need the climate files in the glacier directories.
+gdirs = workflow.init_glacier_directories(rgidf, from_prepro_level=1)
+#Run the prepro tasks on the dem to get flowlines using the generic processing etc. This could be broken down if we don't want certain tasks.
+workflow.gis_prepro_tasks(gdirs)
+#Process the climate data
+log.info('Process the climate data...')
+workflow.execute_entity_task(tasks.process_climate_data, gdirs,y1=2018)
+#Run the climate calibrations based on the new mass balance data, this mass balance data should be in the Working directory folder.
+workflow.execute_entity_task(tasks.local_t_star, gdirs)
+workflow.execute_entity_task(tasks.mu_star_calibration, gdirs)
+#Run the inversion tools ahead of the model runs
+#First set the paramters we can change, currently OGGM default ones:
+# Deformation: from Cuffey and Patterson 2010
+glen_a = 2.4e-24
+# Sliding: from Oerlemans 1997
+fs = 5.7e-20
+#Prep the data for inversion
+workflow.execute_entity_task(tasks.prepare_for_inversion, gdirs)
+#Run the inversion
+workflow.execute_entity_task(tasks.mass_conservation_inversion, gdirs,
+                                 glen_a=glen_a, fs=fs)
+#Apparently can be an issue with the last few grid points being noisy or having a negative slope so can filter these without affecting the total volume
+workflow.execute_entity_task(tasks.filter_inversion_output, gdirs)
+
+#Compile all the previous tasks to give the output ready as input for a model run
+workflow.execute_entity_task(tasks.init_present_time_glacier, gdirs)
+
+
+#Run the model run, this can be changed to 'run_from_climate_data' for our runs
+workflow.execute_entity_task(tasks.run_random_climate, gdirs,
+                             nyears=100, y0=2000, seed=1,store_monthly_step=True,
+                             output_filesuffix='_commitment')
+
+workflow.execute_entity_task(tasks.compile_run_output, gdirs)
+workflow.execute_entity_task(tasks.compile_climate_input, gdirs)
+
+# Log
+m, s = divmod(time.time() - start, 60)
+h, m = divmod(m, 60)
+log.workflow('OGGM is done! Time needed: %d:%02d:%02d' % (h, m, s))
---
+++ data_extraction_scripts/data_extraction_from_per_glacier.py
@@ -0,0 +1,150 @@
+import os as os
+import numpy as np
+import pandas as pd
+import xarray as xr
+
+#Set up the working directory
+run_name='cru_rf_hgt_custom_climate_runs'
+#os.mkdir('/exports/csce/datastore/geos/groups/geos_iceocean/kinnear/oggm_run_data_for_swarm/'+run_name)
+working_dir = '/exports/csce/datastore/geos/groups/geos_iceocean/kinnear/'+run_name
+#Now locate the raw dataset
+run_name = 'oggm_custom_climate_cru_rf_hgt'
+raw_data_directory = '/exports/csce/datastore/geos/groups/geos_iceocean/kinnear/oggm_runs/'
+raw_data_folder = raw_data_directory+run_name+'/per_glacier/'
+#Now give a it an output to make sure it's running properly and you can check
+print('Processing the data from '+raw_data_folder+' to output in '+working_dir)
+#Set up files to opened
+
+rgi_id = []
+num=1
+#Get list of RGI-IDs
+for root,dirs,files in os.walk(raw_data_folder, topdown=False):
+    for name in files:
+        if name == 'model_diagnostics_commitment.nc':
+            path = os.path.join(root,name)
+            rgi = os.path.basename(os.path.dirname(path))
+            rgi = str(rgi)
+            rgi_id.append(rgi)
+#Hacky way to start the process by making a file to input the data scraped from the loop into.
+for root, dirs, files in os.walk(raw_data_folder, topdown=False):
+    for name in files:
+       if num == 1:
+           if name == 'model_diagnostics_commitment.nc':
+              fname = os.path.join(root,name)
+              with xr.open_dataset(fname) as model_run:
+                  time = model_run.time.values
+                  yrs = model_run.hydro_year.values
+                  months = model_run.hydro_month.values
+                  cyrs = model_run.calendar_year.values
+                  cmonths = model_run.calendar_month.values
+
+                  #Create the file for writing
+                  ds = xr.Dataset()
+                  print('opened number: {}'.format(num))
+                  #Global attributes
+                  ds.attrs['description'] = 'OGGM model output'
+                  ds.attrs['calendar'] = '365-day no leap'
+
+
+                  # Coordinates
+                  ds.coords['time'] = ('time', time)
+                  ds.coords['rgi_id'] = ('rgi_id', rgi_id)
+                  ds.coords['hydro_year'] = ('time', yrs)
+                  ds.coords['hydro_month'] = ('time', months)
+                  ds.coords['calendar_year'] = ('time', cyrs)
+                  ds.coords['calendar_month'] = ('time', cmonths)
+                  ds['time'].attrs['description'] = 'Floating hydrological year'
+                  ds['rgi_id'].attrs['description'] = 'RGI glacier identifier'
+                  ds['hydro_year'].attrs['description'] = 'Hydrological year'
+                  ds['hydro_month'].attrs['description'] = 'Hydrological month'
+                  ds['calendar_year'].attrs['description'] = 'Calendar year'
+                  ds['calendar_month'].attrs['description'] = 'Calendar month'
+
+                  #Create the dimensions
+                  shape = (len(time), len(rgi_id))
+                  print('There are {} glaciers that contain run data'.format(len(rgi_id)))
+                  # These variables are always available
+                  vol = np.zeros(shape)
+                  area = np.zeros(shape)
+                  length = np.zeros(shape)
+                  ela = np.zeros(shape)
+                  # These are not
+                  calving_m3 = None
+                  calving_rate_myr = None
+                  volume_bsl_m3 = None
+                  volume_bwl_m3 = None
+                  for i in range (len(rgi_id)):
+                      for root, dirs, files in os.walk(raw_data_folder, topdown=False):
+                          for name in dirs:
+                              if name == rgi_id[i]:
+                                  print('found glacier number: {}'.format(i))
+                                  ppath = os.path.join(root,name,'model_diagnostics_commitment.nc')
+                                  with xr.open_dataset(ppath) as ds_diag:
+                                      vol[:, i] = ds_diag.volume_m3.values
+                                      area[:, i] = ds_diag.area_m2.values
+                                      length[:, i] = ds_diag.length_m.values
+                                      ela[:, i] = ds_diag.ela_m.values
+                                      if 'calving_m3' in ds_diag:
+                                          if calving_m3 is None:
+                                              calving_m3 = np.zeros(shape) * np.NaN
+                                          calving_m3[:, i] = ds_diag.calving_m3.values
+                                      if 'calving_rate_myr' in ds_diag:
+                                          if calving_rate_myr is None:
+                                              calving_rate_myr = np.zeros(shape) * np.NaN
+                                          calving_rate_myr[:, i] = ds_diag.calving_rate_myr.values
+                                      if 'volume_bsl_m3' in ds_diag:
+                                          if volume_bsl_m3 is None:
+                                              volume_bsl_m3 = np.zeros(shape) * np.NaN
+                                          volume_bsl_m3[:, i] = ds_diag.volume_bsl_m3.values
+                                      if 'volume_bwl_m3' in ds_diag:
+                                          if volume_bwl_m3 is None:
+                                              volume_bwl_m3 = np.zeros(shape) * np.NaN
+                                          volume_bwl_m3[:, i] = ds_diag.volume_bwl_m3.values
+
+                  ds['volume'] = (('time', 'rgi_id'), vol)
+                  ds['volume'].attrs['description'] = 'Total glacier volume'
+                  ds['volume'].attrs['units'] = 'm 3'
+                  ds['area'] = (('time', 'rgi_id'), area)
+                  ds['area'].attrs['description'] = 'Total glacier area'
+                  ds['area'].attrs['units'] = 'm 2'
+                  ds['length'] = (('time', 'rgi_id'), length)
+                  ds['length'].attrs['description'] = 'Glacier length'
+                  ds['length'].attrs['units'] = 'm'
+                  ds['ela'] = (('time', 'rgi_id'), ela)
+                  ds['ela'].attrs['description'] = 'Glacier Equilibrium Line Altitude (ELA)'
+                  ds['ela'].attrs['units'] = 'm a.s.l'
+                  if calving_m3 is not None:
+                          ds['calving'] = (('time', 'rgi_id'), calving_m3)
+                          ds['calving'].attrs['description'] = ('Total calving volume since '
+                                                                'simulation start')
+                          ds['calving'].attrs['units'] = 'm3'
+                  if calving_rate_myr is not None:
+                          ds['calving_rate'] = (('time', 'rgi_id'), calving_rate_myr)
+                          ds['calving_rate'].attrs['description'] = 'Instantaneous calving rate'
+                          ds['calving_rate'].attrs['units'] = 'm yr-1'
+                  if volume_bsl_m3 is not None:
+                          ds['volume_bsl'] = (('time', 'rgi_id'), volume_bsl_m3)
+                          ds['volume_bsl'].attrs['description'] = ('Total glacier volume below '
+                                                                   'sea level')
+                          ds['volume_bsl'].attrs['units'] = 'm3'
+                  if volume_bwl_m3 is not None:
+                          ds['volume_bwl'] = (('time', 'rgi_id'), volume_bwl_m3)
+                          ds['volume_bwl'].attrs['description'] = ('Total glacier volume below '
+                                                                   'water level')
+                          ds['volume_bwl'].attrs['units'] = 'm3'
+
+
+                  num = num+1
+                  break
+
+
+       else:
+        break
+#Now output the File
+ds.to_netcdf(path=working_dir+'/test.nc')
+
+
+
+
+
+
