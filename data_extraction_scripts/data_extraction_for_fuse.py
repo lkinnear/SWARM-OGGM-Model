84bb200b597fd01f384335aa9e18feef80a609fa
lkinnear <s0933963@sms.ed.ac.uk>
2020-08-18 15:28:09 +0100

    Getting there with data extraction script

--- data_extraction_scripts/data_extraction_for_fuse.py
+++ data_extraction_scripts/data_extraction_for_fuse.py
@@ -41,6 +41,7 @@ for root, dirs, files in os.walk(raw_data_folder, topdown=False):
 
                   #Create the file for writing
                   ds = xr.Dataset()
+
                   print('opened number: {}'.format(num))
                   #Global attributes
                   ds.attrs['description'] = 'OGGM model output'
@@ -48,8 +49,9 @@ for root, dirs, files in os.walk(raw_data_folder, topdown=False):
 
 
                   # Coordinates
-                  
+
                   ds.coords['time'] = ('time', time)
+                  ds.coords['time'].astype(float)
                   ds.coords['rgi_id'] = ('rgi_id', rgi_id)
                   ds.coords['hydro_year'] = ('time', yrs)
                   ds.coords['hydro_month'] = ('time', months)
@@ -68,6 +70,7 @@ for root, dirs, files in os.walk(raw_data_folder, topdown=False):
                   # These variables are always available
                   vol = np.zeros(shape)
                   area = np.zeros(shape)
+                  precip = np.zeroes(shape)
                   latitude =np.zeros(len(rgi_id))
                   longitude =np.zeros(len(rgi_id))
 
@@ -78,7 +81,7 @@ for root, dirs, files in os.walk(raw_data_folder, topdown=False):
                                   fold = rgi_id[i][:8]
                                   subfold = rgi_id[i][:11]
                                   folpath = raw_data_folder+'/'+fold+'/'+subfold+'/'+rgi_id[i]+'/model_diagnostics_commitment.nc'
-                                  print('found glacier number: {}'.format(i))
+                                  #print('found glacier number: {}'.format(i))
                                   with xr.open_dataset(folpath) as ds_diag:
                                       vol[:, i] = ds_diag.volume_m3.values
                                       area[:, i] = ds_diag.area_m2.values
@@ -90,21 +93,39 @@ for root, dirs, files in os.walk(raw_data_folder, topdown=False):
 
                                       latitude[i] = lat
                                       longitude[i] = lon
+                                      latitude.astype(float)
+                                      longitude.astype(float)
+                                      precip[:, i] = ds_diag.prcp.values
 
                                  #Now get the rainfall
 
 
 
-                  idx = pd.MultiIndex.from_arrays(arrays=[latitude,longitude], names=["lat","lon"])
-                  volume = pd.Series(data=vol, index=idx)
-                  ds['volume'] = xr.DataArray.from_series(volume)
-                  #ds['volume'] = (('lat','lon','time', 'rgi_id'), (lat,lon,vol))
-                  ds['volume'].set_coords('lat','lon')
+
+                  #idx = pd.MultiIndex.from_arrays(arrays=[latitude,longitude], names=["lat","lon"])
+                  #volume = pd.Series(data=vol, index=idx)
+                  #ds['volume'] = xr.DataArray.from_series(volume)
+                  #ds.coords['lat'] = ('lat', latitude)
+                  #ds.coords['lon'] = ('lon', longitude)
+                  #lat = xr.DataArray(latitude, [('latitude', latitude)])
+                  #lon = xr.DataArray(longitude, [('longitude', longitude)])
+                  ds['volume'] = ((('time', 'rgi_id'), vol))
+                  #ds['volume'] = ds['volume'].expand_dims(lon=lon)
+                  #ds['volume'] = ds['volume'].expand_dims(lat=lat)
                   ds['volume'].attrs['description'] = 'Total glacier volume'
                   ds['volume'].attrs['units'] = 'm 3'
+
+
                   ds['area'] = (('time','rgi_id'), area)
                   ds['area'].attrs['description'] = 'Total glacier area'
                   ds['area'].attrs['units'] = 'm 2'
+
+                  ds['precipitation'] = (('time','rgi_id'), precip)
+                  ds['precipitation'].attrs['description'] = 'Total monthly precipitation'
+                  ds['precipitation'].attrs['units'] = 'kg m 2'
+
+
+
 
                   num = num+1
                   break
@@ -112,8 +133,21 @@ for root, dirs, files in os.walk(raw_data_folder, topdown=False):
 
        else:
         break
+#Create a dataframe with the RGI ID and the lat lon Coordinates.
+#Now process the DataFrame
+loc_data = np.stack((rgi_id,latitude,longitude),axis=1)
+loc_df = pd.DataFrame(loc_data,columns=['rgi_id','lat','lon'])
+loc_df["lat"] = pd.to_numeric(loc_df["lat"], downcast="float")
+loc_df["lon"] = pd.to_numeric(loc_df["lon"], downcast="float")
+#Cut this up for binning
+lat_cut = pd.cut(loc_df.lat, np.linspace(16, 57.25, 166),labels=(np.linspace(16, 57, 165)))
+lon_cut = pd.cut(loc_df.lon, np.linspace(72, 143.25, 286),labels=(np.linspace(72, 143, 285)))
+loc_df['lat_bin'] = lat_cut
+loc_df['lon_bin'] = lon_cut
 #Now output the File
-ds.to_netcdf(path=working_dir+'/test.nc')
+loc_df.to_csv(working_dir+'/loc_bins_labeled.csv')
+#df = ds.to_dask_dataframe()
+#ds.to_netcdf(path=working_dir+'/test.nc',mode='w',format='NETCDF4')
 
 
 
